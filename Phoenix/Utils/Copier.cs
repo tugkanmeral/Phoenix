d36768701f5a6c2c2e94d56d03ec2bbc65ee0025
tugkanmeral <tugkan.mrl@gmail.com>
2021-07-18 02:45:08 +0300

    Object Deep and Shallow Copy methods added to utilities

---
+++ Phoenix/Utils/Copier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Phoenix.Utils
+{
+    [Serializable]
+    public class Copier<T> where T : class
+    {
+        public T ShallowCopy()
+        {
+            return (T)this.MemberwiseClone();
+        }
+        public T DeepCopy()
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, this);
+                ms.Position = 0;
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+    }
+}
